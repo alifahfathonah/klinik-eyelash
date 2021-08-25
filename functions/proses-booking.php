7cc33efd5407678f2e0ee16b8af7a924af15e010
nirwannirwan <sihino.miil@gmail.com>
2021-08-25 14:28:47 +0700

    Proses Edit Booking done

--- functions/proses-booking.php
+++ functions/proses-booking.php
@@ -2,11 +2,10 @@
 include "../model/db.php";
 session_start();
 
-if(isset($_POST['submit'])){ 
+if (isset($_POST['submit'])) {
   $nama = $_POST['nama'];
   $no_telp = $_POST['no_telp'];
-  $start = $_POST['start'];
-  $end = $_POST['end'];
+  $start = $_POST['tgl_pasang'];
   $start_jam = $_POST['start_jam'];
   $ends_jam = $_POST['ends_jam'];
   $sumber = $_POST['sumber'];
@@ -23,71 +22,41 @@ if(isset($_POST['submit'])){
   $tgl_lahir = $_POST['tgl_lahir'];
   $status = 1;
 
-  $query = mysqli_query($link,"SELECT a.* FROM users a WHERE a.id_users = '$id_users' ");
+  $query = mysqli_query($link, "SELECT a.* FROM users a WHERE a.id_users = '$id_users' ");
   $username = mysqli_fetch_array($query);
 
-  $cekdulu=  mysqli_query($link,"SELECT * FROM events WHERE start = '$start' ");
+  $cekdulu =  mysqli_query($link, "SELECT * FROM events WHERE start = '$start' ");
   $data = mysqli_fetch_array($cekdulu);
   // var_dump($data['start']);die();
 
-  if ($start == $data['start'] && $ends_jam != $data['ends_jam']) {
-    $query = "INSERT INTO events(nama, no_telp, start, start_jam, ends, ends_jam, sumber, id_produk, id_cabang, harga, id_tipe, id_users, transfer, cash, warna, keterangan, tgl_retouch, status) values ('$nama', '$no_telp', '$start', '$start_jam', '$end', '$ends_jam', '$sumber', '$id_produk', '$id_cabang', '$harga', '$id_tipe', '$id_users', '$transfer', '$cash', '$warna', '$keterangan', '$tgl_retouch', tgl_lahir = '$tgl_lahir', '$status')";
-    $result=mysqli_query($link,$query);
-    $id = mysqli_insert_id($link);
-    // var_dump($result);die();
-    if($result){ // Cek jika proses simpan ke database sukses atau tidak
-      $_SESSION['status'] = "Berhasil";
-      $_SESSION['status_text'] = "Booking Berhasil Di Simpan";
-      $_SESSION['status_code'] = "success";
-      header("location: ../dashboard"); 
-    }else{
-      $_SESSION['status'] = "Gagal";
-      $_SESSION['status_text'] = "Booking Gagal Di Simpan";
-      $_SESSION['status_code'] = "error";
-      header("location: ../dashboard"); 
-    }
-  }elseif ($start == $data['start'] && $ends_jam == $data['ends_jam'] && $id_users != $data['id_users']) {
-    $query = "INSERT INTO events(nama, no_telp, start, start_jam, ends, ends_jam, sumber, id_produk, id_cabang, harga, id_tipe, id_users, transfer, cash, warna, keterangan, tgl_retouch, status) values ('$nama', '$no_telp', '$start', '$start_jam', '$end', '$ends_jam', '$sumber', '$id_produk', '$id_cabang', '$harga', '$id_tipe', '$id_users', '$transfer', '$cash', '$warna', '$keterangan', '$tgl_retouch', tgl_lahir = '$tgl_lahir', '$status')";
-    $result=mysqli_query($link,$query);
-    $id = mysqli_insert_id($link);
-    // var_dump($result);die();
-    if($result){ // Cek jika proses simpan ke database sukses atau tidak
-      $_SESSION['status'] = "Berhasil";
-      $_SESSION['status_text'] = "Booking Berhasil Di Simpan";
-      $_SESSION['status_code'] = "success";
-      header("location: ../dashboard"); 
-    }else{
-      $_SESSION['status'] = "Gagal";
-      $_SESSION['status_text'] = "Booking Gagal Di Simpan";
-      $_SESSION['status_code'] = "error";
-      header("location: ../dashboard"); 
-    }
+  var_dump($start == $data['start'] && $start_jam != $data['start_jam'] && $ends_jam != $data['ends_jam']);
+  die;
 
-  }elseif ($start != $data['start']){
-    $query = "INSERT INTO events(nama, no_telp, start, start_jam, ends, ends_jam, sumber, id_produk, id_cabang, harga, id_tipe, id_users, transfer, cash, warna, keterangan, tgl_retouch, tgl_lahir, status) values ('$nama', '$no_telp', '$start', '$start_jam', '$end', '$ends_jam', '$sumber', '$id_produk', '$id_cabang', '$harga', '$id_tipe', '$id_users', '$transfer', '$cash', '$warna', '$keterangan', '$tgl_retouch', '$tgl_lahir','$status')";
-    $result=mysqli_query($link,$query);
+  if ($start == $data['start'] && $start_jam != $data['start_jam'] && $ends_jam != $data['ends_jam']) {
+    $query = "INSERT INTO events(nama, no_telp, start, start_jam, ends, ends_jam, sumber, id_produk, id_cabang, harga, id_tipe, id_users, transfer, cash, warna, keterangan, tgl_retouch, status) values ('$nama', '$no_telp', '$start', '$start_jam', '$end', '$ends_jam', '$sumber', '$id_produk', '$id_cabang', '$harga', '$id_tipe', '$id_users', '$transfer', '$cash', '$warna', '$keterangan', '$tgl_retouch', tgl_lahir = '$tgl_lahir', '$status')";
+    $result = mysqli_query($link, $query);
     $id = mysqli_insert_id($link);
     // var_dump($result);die();
-    if($result){ // Cek jika proses simpan ke database sukses atau tidak
+    if ($result) { // Cek jika proses simpan ke database sukses atau tidak
       $_SESSION['status'] = "Berhasil";
       $_SESSION['status_text'] = "Booking Berhasil Di Simpan";
       $_SESSION['status_code'] = "success";
-      header("location: ../dashboard"); 
-    }else{
+      header("location: ../dashboard");
+    } else {
       $_SESSION['status'] = "Gagal";
       $_SESSION['status_text'] = "Booking Gagal Di Simpan";
       $_SESSION['status_code'] = "error";
-      header("location: ../dashboard"); 
+      header("location: ../dashboard");
     }
-  }else{
+  } else {
     $_SESSION['status'] = "Gagal";
-    $_SESSION['status_text'] = ' '.$username['username'].' Sudah Mempunyai Jadwal di Jam '.$start_jam.' - '.$ends_jam.' ';
+    $_SESSION['status_text'] = ' ' . $username['username'] . ' Sudah Mempunyai Jadwal di Jam ' . $start_jam . ' - ' . $ends_jam . ' ';
     $_SESSION['status_code'] = "error";
-    header("location: ../dashboard"); 
+    header("location: ../dashboard");
   }
 }
 
-if(isset($_POST['edit'])){ 
+if (isset($_POST['edit'])) {
   $id = $_POST['id'];
   $nama = $_POST['nama'];
   $no_telp = $_POST['no_telp'];
@@ -107,117 +76,75 @@ if(isset($_POST['edit'])){
   $tgl_retouch = $_POST['tgl_retouch'];
   $tgl_lahir = $_POST['tgl_lahir'];
 
-  $query = mysqli_query($link,"SELECT a.* FROM users a WHERE a.id_users = '$id_users' ");
+  $query = mysqli_query($link, "SELECT a.* FROM users a WHERE a.id_users = '$id_users' ");
   $username = mysqli_fetch_array($query);
 
-  $cekdulu=  mysqli_query($link,"SELECT * FROM events WHERE start = '$start' ");
+  $cekdulu =  mysqli_query($link, "SELECT * FROM events WHERE start = '$start'");
   $data = mysqli_fetch_array($cekdulu);
 
-  if ($start == $data['start'] && $ends_jam != $data['ends_jam']) {
+  // echo $start . "<br />";
+  // echo $start_jam . "<br />";
+  // echo $ends_jam . "<br />";
+  // echo $id_users . "<br /><br />";
+
+  // echo $data['start'] . "<br />";
+  // echo $data['start_jam'] . "<br />";
+  // echo $data['ends_jam'] . "<br />";
+  // echo $data['id_users'] . "<br />";
+
+  // echo $start . " == " . $data['start'] . " && " . $start . " != " . $data['start'];
+  // die();
+
+  if ($start == $data['start']) {
     $sql = "UPDATE events SET nama='$nama', no_telp='$no_telp', sumber='$sumber', id_produk='$id_produk', id_cabang='$id_cabang', harga='$harga', id_tipe='$id_tipe', id_users='$id_users', transfer='$transfer', cash='$cash', warna='$warna', keterangan='$keterangan', start_jam = '$start_jam', ends_jam = '$ends_jam', tgl_retouch = '$tgl_retouch', tgl_lahir = '$tgl_lahir' WHERE id = $id";
     $result = mysqli_query($link, $sql);
     // var_dump($result);die();
-    if($result){ // Cek jika proses simpan ke database sukses atau tidak
-          // Jika Sukses, Lakukan :
+    if ($result) { // Cek jika proses simpan ke database sukses atau tidak
+      // Jika Sukses, Lakukan :
       $_SESSION['status'] = "Berhasil";
       $_SESSION['status_text'] = "Booking Berhasil Di Edit";
       $_SESSION['status_code'] = "success";
-      header("location: ../dashboard");   
-
-    }else{
+      header("location: ../booking");
+    } else {
       // Jika Gagal, Lakukan :  
       $_SESSION['status'] = "Gagal";
       $_SESSION['status_text'] = "Booking Gagal Di Edit";
       $_SESSION['status_code'] = "error";
-      header("location: ../dashboard");   
+      header("location: ../booking");
     }
-  }elseif ($start == $data['start'] && $ends_jam == $data['ends_jam'] && $id_users != $data['id_users']) {
-    $sql = "UPDATE events SET nama='$nama', no_telp='$no_telp', sumber='$sumber', id_produk='$id_produk', id_cabang='$id_cabang', harga='$harga', id_tipe='$id_tipe', id_users='$id_users', transfer='$transfer', cash='$cash', warna='$warna', keterangan='$keterangan', start_jam = '$start_jam', ends_jam = '$ends_jam', tgl_retouch = '$tgl_retouch', tgl_lahir = '$tgl_lahir' WHERE id = $id";
-    $result = mysqli_query($link, $sql);
-    // var_dump($result);die();
-    if($result){ // Cek jika proses simpan ke database sukses atau tidak
-          // Jika Sukses, Lakukan :
-      $_SESSION['status'] = "Berhasil";
-      $_SESSION['status_text'] = "Booking Berhasil Di Edit";
-      $_SESSION['status_code'] = "success";
-      header("location: ../dashboard");   
-
-    }else{
-      // Jika Gagal, Lakukan :  
-      $_SESSION['status'] = "Gagal";
-      $_SESSION['status_text'] = "Booking Gagal Di Edit";
-      $_SESSION['status_code'] = "error";
-      header("location: ../dashboard");   
-    }
-  }elseif ($start != $data['start']){
-    $sql = "UPDATE events SET nama='$nama', no_telp='$no_telp', sumber='$sumber', id_produk='$id_produk', id_cabang='$id_cabang', harga='$harga', id_tipe='$id_tipe', id_users='$id_users', transfer='$transfer', cash='$cash', warna='$warna', keterangan='$keterangan', start_jam = '$start_jam', ends_jam = '$ends_jam', tgl_retouch = '$tgl_retouch', tgl_lahir = '$tgl_lahir' WHERE id = $id";
-    $result = mysqli_query($link, $sql);
-    // var_dump($result);die();
-    if($result){ // Cek jika proses simpan ke database sukses atau tidak
-          // Jika Sukses, Lakukan :
-      $_SESSION['status'] = "Berhasil";
-      $_SESSION['status_text'] = "Booking Berhasil Di Edit";
-      $_SESSION['status_code'] = "success";
-      header("location: ../dashboard");   
-
-    }else{
-      // Jika Gagal, Lakukan :  
-      $_SESSION['status'] = "Gagal";
-      $_SESSION['status_text'] = "Booking Gagal Di Edit";
-      $_SESSION['status_code'] = "error";
-      header("location: ../dashboard");   
-    }
-  }elseif ($id_users == $data['id_users'] && $start_jam != $data['start_jam']){
-    $sql = "UPDATE events SET nama='$nama', no_telp='$no_telp', sumber='$sumber', id_produk='$id_produk', id_cabang='$id_cabang', harga='$harga', id_tipe='$id_tipe', id_users='$id_users', transfer='$transfer', cash='$cash', warna='$warna', keterangan='$keterangan', start_jam = '$start_jam', ends_jam = '$ends_jam', tgl_retouch = '$tgl_retouch', tgl_lahir = '$tgl_lahir' WHERE id = $id";
-    $result = mysqli_query($link, $sql);
-    // var_dump($result);die();
-    if($result){ // Cek jika proses simpan ke database sukses atau tidak
-          // Jika Sukses, Lakukan :
-      $_SESSION['status'] = "Berhasil";
-      $_SESSION['status_text'] = "Booking Berhasil Di Edit";
-      $_SESSION['status_code'] = "success";
-      header("location: ../dashboard");   
-
-    }else{
-      // Jika Gagal, Lakukan :  
-      $_SESSION['status'] = "Gagal";
-      $_SESSION['status_text'] = "Booking Gagal Di Edit";
-      $_SESSION['status_code'] = "error";
-      header("location: ../dashboard");   
-    }
-  }else{
+  } else {
     // Jika Gagal, Lakukan :  
     $_SESSION['status'] = "Gagal";
-    $_SESSION['status_text'] = ' '.$username['username'].' Sudah Mempunyai Jadwal di Jam '.$start_jam.' - '.$ends_jam.' ';
+    $_SESSION['status_text'] = ' ' . $username['username'] . ' Sudah Mempunyai Jadwal di Jam ' . $start_jam . ' - ' . $ends_jam . ' ';
     $_SESSION['status_code'] = "error";
-    header("location: ../dashboard"); 
+    header("location: ../booking");
   }
 }
 
-if(isset($_POST['tanya_tanya'])){ 
+if (isset($_POST['tanya_tanya'])) {
   $nama = $_POST['nama'];
   $no_telp = $_POST['no_telp'];
   $sumber = $_POST['sumber'];
   $status = 2;
 
   $query = "INSERT INTO events(nama, no_telp, sumber, status) values ('$nama', '$no_telp', '$sumber','$status')";
-    $result=mysqli_query($link,$query);
-    $id = mysqli_insert_id($link);
-    // var_dump($result);die();
-    if($result){ // Cek jika proses simpan ke database sukses atau tidak
-      $_SESSION['status'] = "Berhasil";
-      $_SESSION['status_text'] = "data Berhasil Di Simpan";
-      $_SESSION['status_code'] = "success";
-      header("location: ../dashboard"); 
-    }else{
-      $_SESSION['status'] = "Gagal";
-      $_SESSION['status_text'] = "data Gagal Di Simpan";
-      $_SESSION['status_code'] = "error";
-      header("location: ../dashboard"); 
-    }
+  $result = mysqli_query($link, $query);
+  $id = mysqli_insert_id($link);
+  // var_dump($result);die();
+  if ($result) { // Cek jika proses simpan ke database sukses atau tidak
+    $_SESSION['status'] = "Berhasil";
+    $_SESSION['status_text'] = "data Berhasil Di Simpan";
+    $_SESSION['status_code'] = "success";
+    header("location: ../dashboard");
+  } else {
+    $_SESSION['status'] = "Gagal";
+    $_SESSION['status_text'] = "data Gagal Di Simpan";
+    $_SESSION['status_code'] = "error";
+    header("location: ../dashboard");
+  }
 }
 
-if(isset($_POST['Event'])){ 
+if (isset($_POST['Event'])) {
   $id = $_POST['Event'][0];
   $start = $_POST['Event'][1];
   $end = $_POST['Event'][2];
@@ -225,11 +152,10 @@ if(isset($_POST['Event'])){
   $sql = "UPDATE events SET  start = '$start', ends = '$end' WHERE id = $id ";
   $result = mysqli_query($link, $sql);
   // var_dump($result);die();
-  if($result){ // Cek jika proses simpan ke database sukses atau tidak
-        // Jika Sukses, Lakukan :
+  if ($result) { // Cek jika proses simpan ke database sukses atau tidak
+    // Jika Sukses, Lakukan :
     echo "sukses";
-
-  }else{
+  } else {
     echo "Gagal";
   }
 }
