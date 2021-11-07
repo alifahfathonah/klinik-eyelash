115233a1e8259dd9d92fcd570a50772a4191d03f
rioagf <rioagungfirmansyah@gmail.com>
2021-11-08 02:40:25 +0700

    update Nama Cabang

--- dashboard.php
+++ dashboard.php
@@ -200,7 +200,7 @@ if (isset($_GET['search'])) {
                                                                 <?php if ($data['tanggal'] == $tgl) { ?>
                                                                     <td style="color:green; font-weight:900; text-align:center;"><a href="#" data-toggle="modal" data-target="#ModalEdit-<?= $data['id'] ?>" style="color: green"><?php echo $data['jam'] ?></a></td>
                                                                 <?php } else { ?>
-                                                                    <td style="color:red; font-weight:900; text-align:center;"><a href="booking?cabang=<?php echo $qc['nama_cabang'] ?>&jam=<?php echo $data['jam'] ?>&tanggal=<?= $tanggal_sekarang ?>&id_jabatan=1&popup=1" target="_blank" style="color: red;"><?php echo $data['jam'] ?></a></td>
+                                                                    <td style="color:red; font-weight:900; text-align:center;"><a href="booking?cabang=<?php echo $nama_cbg ?>&jam=<?php echo $data['jam'] ?>&tanggal=<?= $tanggal_sekarang ?>&id_jabatan=1&popup=1" target="_blank" style="color: red;"><?php echo $data['jam'] ?></a></td>
                                                                 <?php } ?>
                                                             </tr>
                                                         <?php } ?>
@@ -232,7 +232,7 @@ if (isset($_GET['search'])) {
                                                                 <?php if ($data['tanggal'] == $tgl) { ?>
                                                                     <td style="color:green; font-weight:900; text-align:center;"><a href="#" data-toggle="modal" data-target="#ModalEdit-<?= $data['id'] ?>" style="color: green"><?php echo $data['jam'] ?></a></td>
                                                                 <?php } else { ?>
-                                                                    <td style="color:red; font-weight:900; text-align:center;"><a href="booking?cabang=<?php echo $qc['nama_cabang'] ?>&jam=<?php echo $data['jam'] ?>&tanggal=<?= $tanggal_sekarang ?>&id_jabatan=1&popup=1" target="_blank" style="color: red;"><?php echo $data['jam'] ?></a></td>
+                                                                    <td style="color:red; font-weight:900; text-align:center;"><a href="booking?cabang=<?php echo $nama_cbg ?>&jam=<?php echo $data['jam'] ?>&tanggal=<?= $tanggal_sekarang ?>&id_jabatan=1&popup=1" target="_blank" style="color: red;"><?php echo $data['jam'] ?></a></td>
                                                                 <?php } ?>
                                                             </tr>
                                                         <?php } ?>
